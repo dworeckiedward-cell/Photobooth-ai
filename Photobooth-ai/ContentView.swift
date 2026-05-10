e4e8a435ccbcae14ab7ba32ddfd79164024fa822
Servify <servifyhub.contact@gmail.com>
2026-05-10 13:17:44 +0200

    Initial Commit

---
+++ Photobooth-ai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Photobooth-ai
+//
+//  Created by Mr E. Dworecki on 10/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Photobooth-ai/Photobooth_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photobooth_aiApp.swift
+//  Photobooth-ai
+//
+//  Created by Mr E. Dworecki on 10/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Photobooth_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
